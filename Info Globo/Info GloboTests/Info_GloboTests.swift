06e7f4c4588d28f2495550d2e4658fcca7df7ac1
Fabio Miciano <Miciano@MacBook-Pro-de-Fabio.local>
2017-05-16 16:22:19 -0300

    Testes unitarios concluidos

--- Info Globo/Info GloboTests/Info_GloboTests.swift
+++ Info Globo/Info GloboTests/Info_GloboTests.swift
@@ -8,7 +8,9 @@
 
 import XCTest
 
-class Info_GloboTests: XCTestCase {
+@testable import Info_Globo
+
+class Info_GloboTests: XCTestCase, MainParse {
     
     override func setUp() {
         super.setUp()
@@ -23,6 +25,30 @@ class Info_GloboTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //CARREGO E TESTO O JSON CORRETO
+        let jsonCorrect = loadJsonCorrect()
+        XCTAssertNotNil(jsonCorrect, "Falha no load do arquivo local do JSON correto")
+        let contentCorrect = parserContent(response: jsonCorrect)
+        XCTAssertNotNil(contentCorrect, "Parse do JSON correto falhou")
+        //SE ESPERA QUE RETORNE UM ARRAY DE CONTEUDO COM PELOMENOS 1 ELEMENTO
+        XCTAssert(contentCorrect!.count > 0 ? true : false, "Retornou um modelo vazio ou inválido mesmo com o json correto")
+        
+        //CARREGO E TESTO O JSON VAZIO
+        let jsonEmpty = loadJsonEmpty()
+        XCTAssertNotNil(jsonEmpty, "Falha no load do arquivo local do JSON vazio")
+        let contentEmpty = parserContent(response: jsonEmpty)
+        XCTAssertNotNil(contentEmpty, "Parse do JSON vazio falhou")
+        //SE ESPERA QUE RETORNE UM ARRAY DE CONTEUDO VAZIO
+        XCTAssert(contentEmpty!.count == 0 ? true : false, "Retornou um modelo válido mesmo JSON estando vazio")
+        
+        //CARREGO E TESTO O JSON QUEBRADO
+        let jsonBroken = loadJsonBroken()
+        XCTAssertNotNil(jsonBroken, "Falha no load do arquivo local do JSON Errado")
+        let contentBroken = parserContent(response: jsonBroken)
+        XCTAssertNotNil(contentBroken, "Parse do JSON errado falhou")
+        //SE ESPERA QUE RETORNE UM ARRAY DE CONTEUDO VAZIO
+        XCTAssert(contentBroken!.count == 0 ? true : false, "Retornou um modelo válido mesmo JSON estando errado")
     }
     
     func testPerformanceExample() {
@@ -32,4 +58,52 @@ class Info_GloboTests: XCTestCase {
         }
     }
     
+    //PRAGMA MARK: -- LOADS DE JSONS --
+    //Json correto
+    private func loadJsonCorrect()-> [[String: Any]]? {
+        do {
+            //Carrego o bundle de teste
+            let bundle = Bundle(for: Info_GloboTests.self)
+            guard let file = bundle.url(forResource: "noticesCorrect", withExtension: "json") else { return nil }
+            let data = try Data(contentsOf: file)
+            guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else { return nil }
+            
+            return json
+        }
+        catch {
+            return nil
+        }
+    }
+    
+    //Json vazio
+    private func loadJsonEmpty()-> [[String: Any]]? {
+        do {
+            //Carrego o bundle de teste
+            let bundle = Bundle(for: Info_GloboTests.self)
+            guard let file = bundle.url(forResource: "noticesEmpty", withExtension: "json") else { return nil }
+            let data = try Data(contentsOf: file)
+            guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else { return nil }
+            
+            return json
+        }
+        catch {
+            return nil
+        }
+    }
+    
+    //Json quebrado
+    private func loadJsonBroken()-> [[String: Any]]? {
+        do {
+            //Carrego o bundle de teste
+            let bundle = Bundle(for: Info_GloboTests.self)
+            guard let file = bundle.url(forResource: "noticesBroken", withExtension: "json") else { return nil }
+            let data = try Data(contentsOf: file)
+            guard let json = try JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else { return nil }
+            
+            return json
+        }
+        catch {
+            return nil
+        }
+    }
 }
